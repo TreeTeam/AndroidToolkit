9973b3477732d9d07112564648bd209a936505c7
Phong Phan <phong.phan@apps-cyclone.com>
2014-10-03 15:38:57 +0700

    UPDATE

---
+++ AndroidTookit/KeyWord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+    public class KeyWord
+    {
+        public static int TEXTVIEW = 1;
+        public static int LINEARLAYOUT = 2;
+        public static int IMAGEVIEW = 3;
+        public static int RELATIVELAYOUT = 4;
+        public static int FRAMELAYOUT = 5;
+        public static int LISTVIEW = 6;
+        public static int EDITTEXT = 7;
+        public static int TABLELAYOUT = 8;
+        public static int TABLEROW = 9;
+        public static int GRIDLAYOUT = 10;
+        public static int GRIDVIEW = 11;
+        public static int SCROLLVIEW = 12;
+        public static int HORIZONTALSCROLLVIEW = 13;
+        public static int WEBVIEW = 14;
+        public static int EXPANDABLELISTVIEW = 15;
+        public static int SPINNER = 16;
+        public static int AUTOCOMPLETETEXTVIEW = 17;
+        public static int MULTIAUTOCOMPLETETEXTVIEW = 18;
+        public static int BUTTON = 19;
+        public static int SEEKBAR = 20;
+        public static int PROGRESSBAR = 21;
+        public static int VIEWFLIPPER = 22;
+    }
